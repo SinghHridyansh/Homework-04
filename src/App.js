e16bef9716982e3d4932b94d5521a4001244f1f0
SinghHridyansh <Hridyansh.360@gmail.com>
2023-04-21 15:04:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <div className="top">
+          <img
+            className="userimg"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/55758/random-user-31.jpg"
+            alt="."
+          />
+          <h1>Sara Jay</h1>
+          <p>I enjoy drinking a cup of coffee every day</p>
+        </div>
+        <div className="middle">
+          <div className="numbers">
+            <span className="hglt">172</span>
+            <br />
+            <span className="txt">POSTS</span>
+          </div>
+          <div className="numbers">
+            <span className="hglt">47</span>
+            <br />
+            <span className="txt">FOLLOWERS</span>
+          </div>
+          <div className="numbers">
+            <span className="hglt">20</span>
+            <br />
+            <span className="txt">FOLLOWING</span>
+          </div>
+        </div>
+        <div className="bottom">
+          <button className="purple">FOLLOW</button>
+          <button className="pink">MESSAGE</button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
